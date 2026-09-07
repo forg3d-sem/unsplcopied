2ab32b621c5f5d19d74f7723572a2ab8489d8489
Roman Semeniuk <semyonych@gmail.com>
2026-09-07 18:25:06 +0300

    Added global error page;

---
+++ app/error.tsx
@@ -0,0 +1,35 @@
+'use client';
+import React from 'react';
+import Link from "next/link";
+
+const MainErrorPage = ({error, retry}: {
+    error: Error & { digest?: string }
+    retry: () => void
+}) => {
+    return (
+        <div className='error'>
+            <h1>
+                Error fetching data:
+            </h1>
+            <p>
+                {error.message}
+            </p>
+            <div className='buttons'>
+                <button
+                    className="button primary"
+                    onClick={retry}
+                >
+                    Retry
+                </button>
+                <Link
+                    className='button secondary'
+                    href={'/'}
+                >
+                    Home
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default MainErrorPage;
